00dc810e40024786c37b18b5927f58e3bf785754
tiankaidi <980527417@qq.com>
2021-03-25 22:10:47 +0800

    对首页底部做了修改

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,38 +2,38 @@ $(function () {
   getUserData()
   var layer = layui.layer
 
-  $('#sign_out').click(function () {
-    layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function (index) {
-      localStorage.removeItem('token')
+  $("#sign_out").click(function () {
+    layer.confirm("确定退出登录？", { icon: 3, title: "提示" }, function (index) {
+      localStorage.removeItem("token")
       location.href = "/login.html"
-      layer.close(index);
-    });
+      layer.close(index)
+    })
   })
 })
 
 function getUserData() {
   $.ajax({
-    method: 'GET',
-    url: '/my/userinfo',
+    method: "GET",
+    url: "/my/userinfo",
     success: function (res) {
       if (res.status !== 0) {
-        return layui.layer.msg('获取用户信息失败！')
+        return layui.layer.msg("获取用户信息失败！")
       }
-      console.log(res.data);
+      console.log(res.data)
       renderAvatar(res.data)
-    }
+    },
   })
 }
 
 function renderAvatar(user) {
   var name = user.nickname || user.username
-  $('.welcome').html('欢迎您&nbsp;&nbsp;' + name)
+  $(".welcome").html("欢迎您&nbsp;&nbsp;" + name)
   if (user.user_pic !== null) {
-    $('.layui-nav-img').attr('src', user.user_pic).show()
-    $('.text_hp').hide()
+    $(".layui-nav-img").attr("src", user.user_pic).show()
+    $(".text_hp").hide()
   } else {
     var frist = user.username[0].toUpperCase()
-    $('.layui-nav-img').hide()
-    $('.text_hp').html(frist).show()
+    $(".layui-nav-img").hide()
+    $(".text_hp").html(frist).show()
   }
 }
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,59 +1,57 @@
 $(function () {
-  $('#toReg').on('click', function () {
-    $('.login_box').hide()
-    $('.reg_box').show()
+  $("#toReg").on("click", function () {
+    $(".login_box").hide()
+    $(".reg_box").show()
   })
 
-  $('#toLogin').on('click', function () {
-    $('.login_box').show()
-    $('.reg_box').hide()
+  $("#toLogin").on("click", function () {
+    $(".login_box").show()
+    $(".reg_box").hide()
   })
 
   var form = layui.form
   var layer = layui.layer
   form.verify({
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
     repwd: function (value) {
-      let pwd = $('.reg_box [name=password]').val()
+      let pwd = $(".reg_box [name=password]").val()
       if (pwd !== value) {
-        return '两次密码输入不一致！'
+        return "两次密码输入不一致！"
       }
-    }
+    },
   })
 
-
-  $('#form_reg').submit(function (e) {
-    e.preventDefault();
+  $("#form_reg").submit(function (e) {
+    e.preventDefault()
     var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
+      username: $("#form_reg [name=username]").val(),
+      password: $("#form_reg [name=password]").val(),
     }
-    $.post('/api/reguser',
-      data, function (res) {
-        if (res.status !== 0) {
-          return layer.msg(res.message)
-        }
-        layer.msg('注册成功，请登录！')
-        $('#toLogin').click()
-      })
-  });
+    $.post("/api/reguser", data, function (res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message)
+      }
+      layer.msg("注册成功，请登录！")
+      $("#toLogin").click()
+    })
+  })
 
-  $('#form_login').submit(function (e) {
-    e.preventDefault();
+  $("#form_login").submit(function (e) {
+    e.preventDefault()
     $.ajax({
       method: "POST",
-      url: '/api/login',
+      url: "/api/login",
       data: $(this).serialize(),
 
       success: function (res) {
-        console.log(res);
+        console.log(res)
         if (res.status !== 0) {
-          return layer.msg('登录失败！')
+          return layer.msg("登录失败！")
         }
-        layer.msg('登录成功！')
-        localStorage.setItem('token', res.token)
-        location = 'index.html'
-      }
-    });
+        layer.msg("登录成功！")
+        localStorage.setItem("token", res.token)
+        location = "index.html"
+      },
+    })
   })
 })
